be52004803448a1c91f2088578300394302e8500
gktirkha <gktirkha@gmail.com>
2026-04-02 13:01:45 +0530

    feat(bootstrap): enhance bootstrap template with debouncer import and logging

--- lib/src/templates/bootstrap_dart_template.dart
+++ lib/src/templates/bootstrap_dart_template.dart
@@ -1,14 +1,17 @@
 String bootstrapDartTemplate(String pkg) => '''
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../core/utils/functions/debouncer.dart';
+import '../generated/locales/locales.g.dart';
 import 'di/di_container.dart';
 import 'di/di_initializer.dart';
-import '../generated/locales/locales.g.dart';
 import 'main_app.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
   LocaleSettings.useDeviceLocale();
+  Debouncer.showLogs = kDebugMode;
   await diInitializer(diContainer);
   runApp(const MainApp());
 }
