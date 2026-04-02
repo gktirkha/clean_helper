71d6abe61ac8ec5b86c2dcd4210c9c28680b7b76
gktirkha <gktirkha@gmail.com>
2026-04-02 13:30:09 +0530

    feat(logger): add logger instance to CoreModule for improved logging

--- lib/src/templates/core_module_template.dart
+++ lib/src/templates/core_module_template.dart
@@ -1,5 +1,6 @@
 String coreModuleTemplate() => '''
 import 'package:injectable/injectable.dart';
+import 'package:logger/web.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 @module
@@ -7,5 +8,8 @@ abstract class CoreModule {
   @preResolve
   @lazySingleton
   Future<PackageInfo> get packageInfo => PackageInfo.fromPlatform();
+
+  @lazySingleton
+  Logger get logger => Logger();
 }
 ''';
