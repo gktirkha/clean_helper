dceff72a9e5d3b10936fe35ee0ad9c6dce715538
gktirkha <gktirkha@gmail.com>
2026-04-15 16:07:51 +0530

    fix: correct log level methods in AppLogger template

--- lib/src/templates/app_logger_template.dart
+++ lib/src/templates/app_logger_template.dart
@@ -6,10 +6,10 @@ import '../../../app/di/di_container.dart';
 class AppLogger {
   static Logger get _logger => diContainer();
   static final trace = _logger.t;
-  static final debug = _logger.t;
-  static final info = _logger.t;
-  static final warning = _logger.t;
-  static final error = _logger.t;
+  static final debug = _logger.d;
+  static final info = _logger.i;
+  static final warning = _logger.w;
+  static final error = _logger.e;
   static final what = _logger.w;
 }
 ''';
