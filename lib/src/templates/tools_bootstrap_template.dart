ee88b4da4b07d4766465f7a2a6dd24df95366798
gktirkha <gktirkha@gmail.com>
2026-04-15 12:00:20 +0530

    refactor: make clean optional via --clean flag in bootstrap

--- lib/src/templates/tools_bootstrap_template.dart
+++ lib/src/templates/tools_bootstrap_template.dart
@@ -2,13 +2,15 @@ String toolsBootstrapTemplate() => '''
 import 'clean.dart';
 import 'command_runner.dart';
 
-Future<void> main() async {
-  await clean();
+Future<void> main(List<String> args) async {
+  if (args.contains('--clean')) await clean();
   final hasFvm = await fvmExists();
   final prefix = hasFvm ? 'fvm ' : '';
   if (hasFvm) await commandRunner('fvm use --skip-pub-get');
   await commandRunner('\${prefix}flutter pub get');
   await commandRunner('\${prefix}dart run slang');
-  await commandRunner('\${prefix}dart run build_runner build --delete-conflicting-outputs');
+  await commandRunner(
+    '\${prefix}dart run build_runner build --delete-conflicting-outputs',
+  );
 }
 ''';
