d26c701fb53ba2075d6bedc16393c9f7d298ded3
gktirkha <gktirkha@gmail.com>
2026-05-01 12:24:24 +0530

    refactor: simplify feature page template scaffold structure

--- lib/src/templates/feature_page_template.dart
+++ lib/src/templates/feature_page_template.dart
@@ -6,15 +6,11 @@ import '../../router/${feature}_navigation.dart';
 
 class ${className}Page extends StatelessWidget {
   const ${className}Page({super.key, required this.navigation});
-
   final ${className}Navigation navigation;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('$className')),
-      body: const Center(child: Text('$className Page')),
-    );
+    return const Scaffold(body: Center(child: Text('$className Page')));
   }
 }
 ''';
